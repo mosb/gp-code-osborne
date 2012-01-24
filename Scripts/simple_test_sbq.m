40b8075b392380b53e6f478727e8d1d044236f89
duvenaud@gmail.com <duvenaud@gmail.com@81f0b17c-d628-29b2-8be0-6e4c56917c87>
2012-01-24 16:12:32 +0000

    Small fixes to make repmat calls compatible with matlab 2011.  Added comments to simple_test_sbq.m

--- Scripts/simple_test_sbq.m
+++ Scripts/simple_test_sbq.m
@@ -1,12 +1,12 @@
 
+% Set up a simple toy function to test SQB.
 r_mean1 = 1;
 r_sd1 = 1;
-
 r_mean2 = 4;
 r_sd2 = 1;
-
 log_r_fn = @(x) log(normpdf(x,r_mean1,r_sd1)+normpdf(x,r_mean2,r_sd2));
 
+% Set parameters of SBQ
 start_pt = -3;
 prior_struct.means = 0;
 prior_struct.sds = 1;
@@ -18,13 +18,15 @@ opt.plots = true;
 [samples_mat, log_ev, r_gp] = ...
     sbq(start_pt, log_r_fn, prior_struct, opt);
 
-clf
+% Plot integrand and sample points.
 test_pts = linspace(prior_struct.means - 5*prior_struct.sds, ...
             prior_struct.means + 5*prior_struct.sds, 1000);
-plot(test_pts, log_r_fn(test_pts), 'b')
-hold on
-plot(samples_mat, log_r_fn(samples_mat), '.k', 'MarkerSize', 8)
-xlim([-5 5])
+figure;
+h_func = plot(test_pts, log_r_fn(test_pts), 'b'); hold on;
+h_samples = plot(samples_mat, log_r_fn(samples_mat), '.k', 'MarkerSize', 8)
+xlim([-5 5]);
+legend( [h_func, h_samples], {'Log-integrand', 'Sample Points'}, 'Location', 'Best');
+
 
 % the exact log-evidence
 exact = ...
