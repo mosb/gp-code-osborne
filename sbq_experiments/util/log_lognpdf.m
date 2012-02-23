43a0c05b13cb303106cebb1f89d57b474bfa150b
electronic.oz@gmail.com <electronic.oz@gmail.com@81f0b17c-d628-29b2-8be0-6e4c56917c87>
2012-02-23 16:23:32 +0000

    reverted changes to log_lognpdf, sorry Dave

--- sbq_experiments/util/log_lognpdf.m
+++ sbq_experiments/util/log_lognpdf.m
@@ -1,10 +1,8 @@
 function y = log_lognpdf(z_p,z_mu,z_sigma_squared)
 % y = log_lognpdf(z_p,z_mu,z_sigma_squared)
-% approximate the distribution over a log-transformed variable z
+% use the distribution over a log-transformed variable z
 % p(z) = N(z; z_mu, z_sigma_squared)
-% so as to give a Gaussian over the untransformed variable
-% p(x) = N(x; x_mu, x_sigma_squared)
-% and evaluate the log-density at exp(z_p).
+% to evaluate the log-density of the untransformed variable x at exp(z_p).
 
 if nargin<1
     error(message('stats:lognpdf:TooFewInputs'));
@@ -16,13 +14,12 @@ if nargin < 3
     z_sigma_squared = 1;
 end
 
-x_p = exp(z_p);
-
 % Return NaN for out of range parameters.
 z_sigma_squared(z_sigma_squared <= 0) = NaN;
 
-x_mu = exp(z_mu + 0.5 * z_sigma_squared);
-x_sigma_squared = exp(2 * z_mu) * ...
-            (exp(2 * z_sigma_squared) - exp(z_sigma_squared));
-
-y = lognpdf(x_p, x_mu, x_sigma_squared);
+try
+    y = -0.5 * ((z_p - z_mu).^2)./sigma_squared ...
+        - z_p - log(sqrt(2*pi)) - 0.5*log(z_sigma_squared);
+catch
+    error(message('stats:lognpdf:InputSizeMismatch'));
+end
